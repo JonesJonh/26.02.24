6f04b8d12f60fb6c8e51618201f2e42b474c28dd
JonesJonh <143019996+JonesJonh@users.noreply.github.com>
2024-02-26 12:31:10 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+// A linha de código var x; declara uma variável chamada x em JavaScript, inicializando-a com o valor igual a 6 fechando em ";"
+var x = 6;
+
+// A linha de código var y; declara uma variável chamada y em JavaScript, inicializando-a com o valor igual a 6 fechando em ";"
+var y = 6;
+
+// é uma estrutura de controle de fluxo que permite executar um bloco de código se uma condição for verdadeira (if) e outro bloco de código se a condição for falsa (else).
+if (6 == 6) {
+
+    // A linha de código console.log("Números conferem!"); exibe a mensagem "Números conferem!" no console do navegador ou ambiente de execução JavaScript. É útil para depurar e monitorar o fluxo do programa.
+    console.log("Números conferem!");
+} else {
+    console.log("Números divergem!");
+}
